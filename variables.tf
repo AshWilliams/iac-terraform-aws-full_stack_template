680a0ff04c10b47ad6b9e1674829ac4bf9f60acb
bridgemanart <domains@bridgemanart.co.uk>
2016-09-26 11:19:15 +0100

    Add User Variables

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_access_key" {
+    decscription = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+    description = "AWS Secret Key"
+}
+
+variable "aws_region" {
+    description = "Which AWS Region will be used ?"
+    default = "eu-west-1"
+}
