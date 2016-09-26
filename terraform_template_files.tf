64467bf6b2374ede36e7a7b250e7a4e125587d3f
bridgemanart <domains@bridgemanart.co.uk>
2016-09-26 14:32:49 +0100

    FIX ERROR: file: open /terraform/aws.ec2_instance-userdata.txt: no such file or directory in: ${file("${path.module}/aws.ec2_instance-user..

--- terraform_template_files.tf
+++ terraform_template_files.tf
@@ -1,3 +1,3 @@
 resource "template_file" "ec2_instance-userdata" {
-  template = "${file("${path.module}/aws.ec2_instance-userdata.txt")}"
+  template = "${file("aws.ec2_instance-userdata.txt")}"
 }
