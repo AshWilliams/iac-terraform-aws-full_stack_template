03a573afdd1236a9902ccce5f83d8f15198c59f9
bridgemanart <domains@bridgemanart.co.uk>
2016-09-26 13:15:25 +0100

    FIX ERROR: aws_iam_user_policy.iam_plc-gemv2_zip_ms: "user": required field is not set

--- aws.iam_user_policies.tf
+++ aws.iam_user_policies.tf
@@ -1,5 +1,6 @@
 resource "aws_iam_user_policy" "iam_plc-gemv2_zip_ms" {
     name = "cup-gemv2-zip_ms-ec2"
+	user = "${aws_iam_user.iam_usr-gemv2_zip_ms.name}"
     policy = <<EOF
 {
   "Version": "2012-10-17",
