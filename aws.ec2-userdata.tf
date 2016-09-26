888aa1bb1b6283eedb3700dfb41cf1188a7fd8dc
bridgemanart <domains@bridgemanart.co.uk>
2016-09-26 14:26:27 +0100

    Create Specific TemplateFiles handler

--- aws.ec2-userdata.tf
+++
@@ -1,3 +0,0 @@
-resource "template_file" "ec2-userdata" {
-  template = "${file("${path.module}/aws.ec2-userdata.txt")}"
-}
--- aws.ec2_instances.tf
+++ aws.ec2_instances.tf
@@ -1,9 +1,15 @@
-resource "aws_instance" "ec2_inst-gemv2-zip_ms" {
+resource "aws_instance" "ec2_instance" {
     ami = "ami-7abd0209"
 	
     instance_type = "t2.micro"
 	
-	user_data = "${template_file.ec2-userdata.rendered}"
+	user_data = "${template_file.ec2_instance-userdata.rendered}"
+
+	root_block_device {
+		volume_type		= "gp2"
+		volume_size		= "${var.ec2_instance-root_volume_size}"
+		delete_on_termination	= true
+	}
 	
     tags {
         Name = "GEMv2.ZIP.ms"
