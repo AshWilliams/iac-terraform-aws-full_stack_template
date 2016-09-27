345591322f21619ce652ae8071cdaa58767eff18
bridgemanart <domains@bridgemanart.co.uk>
2016-09-27 10:02:43 +0100

    Adding Variables: project name, ecosystem, webapplication, interface, environment

--- variables.tf
+++ variables.tf
@@ -11,15 +11,34 @@ variable "aws_region" {
     default = "eu-west-1"
 }
 
-variable "ec2_instance-root_volume_size" {
-  description = "Size (in GB) of the Root Volume for the EC2 Instances"
-  default = 10
-}
-
-variable "webapp_name" {
-    description = "Name of the Web Application for which the Infrastructure is about"
-}
 
 variable "ec2_keypair_public" {
     description = "SSH Public Key that will be used to access the EC2 Instances (Example: ssh-rsa AAABBBCCC999... email@example.com)"
+}
+
+
+variable "project_name" {
+    description = "Name of the Project for which the Infrastructure is about | (Example: GEMv2.GLM.app || HTTP || PROD)"
+}
+
+variable "project_webapplication" {
+    description = "Name of the Web Application | (Example: glm.app)"
+}
+
+variable "project_ecosystem" {
+    description = "Name of the Ecosystem, the Web Application is part of | [Options: gemv1 / gemv2]"
+}
+
+variable "project_interface" {
+    description = "Which interface will the Web Application be used through | [Options: http / cli]"
+}
+
+variable "project_environment" {
+    description = "On which Environment will the Web Application be launched | [Options: prod | beta | stg]"
+}
+
+
+variable "ec2_instance-root_volume_size" {
+  description = "Size (in GB) of the Root Volume for the EC2 Instances"
+  default = 10
 }
