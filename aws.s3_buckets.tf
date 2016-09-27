6e577c0d4405ca2443494e5df00f761227d8e2ac
bridgemanart <domains@bridgemanart.co.uk>
2016-09-27 16:54:14 +0100

    FIX ERROR: aws_s3_bucket.prj-s3_bucket-elb_logs: Error creating S3 bucket: InvalidBucketName: The specified bucket is not valid.

--- aws.s3_buckets.tf
+++ aws.s3_buckets.tf
@@ -1,7 +1,8 @@
 resource "aws_s3_bucket" "prj-s3_bucket-elb_logs" {
 
-	bucket = "logs-ec2_elb-${var.project_ecosystem}-${var.project_webapplication}"
+	bucket = "logs-ec2-elb-${var.project_ecosystem}-${var.project_webapplication}"
 	#(Required) The name of the bucket.
+	#A bucket name cannot contain the character '_'.
 	
 	acl = "log-delivery-write"
 	#(Optional) The canned ACL to apply. Defaults to "private".
