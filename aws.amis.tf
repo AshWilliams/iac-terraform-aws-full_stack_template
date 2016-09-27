bb8ddd5d07dfd2b8e59c93fd393a8de7c245021b
bridgemanart <domains@bridgemanart.co.uk>
2016-09-27 15:34:49 +0100

    FIX ERROR: data.aws_ami.centos_ami: Your query returned no results. Please change your filters and try again.

--- aws.amis.tf
+++ aws.amis.tf
@@ -21,10 +21,10 @@ data "aws_ami" "centos_ami" {
 		name = "virtualization-type"
 		values = ["hvm"]
 	}
-	filter {
-		name = "owner-id"
-		values = ["679593333241"]
-	}
+	#filter {
+	#	name = "owner-id"
+	#	values = ["679593333241"]
+	#}
 	filter {
 		name = "description"
 		values = ["CentOS Linux 7*"]
