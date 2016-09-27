3dcccdd67a51d66307d7a74bbcbeb17f231c3dc0
bridgemanart <domains@bridgemanart.co.uk>
2016-09-27 15:47:06 +0100

    * data.aws_ami.centos_ami: Your query returned no results. Please change your filters and try again.

--- aws.amis.tf
+++ aws.amis.tf
@@ -3,12 +3,16 @@ data "aws_ami" "centos_ami" {
 	most_recent = true
 	#(Optional) If more than one result is returned, use the most recent AMI.
 	
-	executable_users = ["self"]
+	#executable_users = ["self"]
 	#(Optional) Limit search to users with explicit launch permission on the image. Valid items are the numeric account ID or self.
 	
 	#filter {}
 	#(Optional) One or more name/value pairs to filter off of. There are several valid keys, for a full reference, check out describe-images in the AWS CLI reference.
 
+	filter {
+		name = "name"
+		values = ["CentOS Linux 7 x86_64*"]
+	}
 	filter {
 		name = "platform"
 		values = ["Cent OS"]
@@ -20,10 +24,6 @@ data "aws_ami" "centos_ami" {
 	filter {
 		name = "virtualization-type"
 		values = ["hvm"]
-	}
-	filter {
-		name = "name"
-		values = ["CentOS Linux 7 x86_64*"]
 	}
 	filter {
 		name = "description"
