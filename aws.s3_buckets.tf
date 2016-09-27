3ee451efcd149409a0919acc1ca26c71a4e899d3
bridgemanart <domains@bridgemanart.co.uk>
2016-09-27 17:39:51 +0100

    Allow force_destroy on S3 Buckets

--- aws.s3_buckets.tf
+++ aws.s3_buckets.tf
@@ -6,4 +6,7 @@ resource "aws_s3_bucket" "prj-s3_bucket-elb_logs" {
 	
 	acl = "private"
 	#(Optional) The canned ACL to apply. Defaults to "private".
+	
+	force_destroy = "true"
+	#(Optional, Default:false ) A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable.
 }
