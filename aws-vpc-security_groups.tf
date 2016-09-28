0530b6b4954cdd23f7beb1b60475503ec08076b3
Georgios Fisaris <gfisaris@gmail.com>
2016-09-28 11:47:48 +0100

    Update help information

--- aws-vpc-security_groups.tf
+++ aws-vpc-security_groups.tf
@@ -1,5 +1,8 @@
 /*
-resource "aws_security_group" "demo-sg" {
+AWS_SECURITY_GROUP
+Provides a security group resource.
+
+resource "aws_security_group" "vpc-sg-demo" {
 
 	#name = ""
 	#(Optional, Forces new resource) The name of the security group. If omitted, Terraform will assign a random, unique name
@@ -17,30 +20,41 @@ resource "aws_security_group" "demo-sg" {
 	#(Optional) A mapping of tags to assign to the resource.
 	
 }
+
+Attributes Reference
+The following attributes are exported:
+
+	id			- The ID of the security group
+	vpc_id		- The VPC ID.
+	owner_id	- The owner ID.
+	name		- The name of the security group
+	description	- The description of the security group
+	ingress		- The ingress rules. See above for more.
+	egress		- The egress rules. See above for more.
 */
 
 resource "aws_security_group" "vpc-sg-ec2-elb" {
-	name = "vpc-sg-ec2-elb-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
-	description = "VPC Security Group controlling access to Project (${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}) Elastic LoadBalancer (${aws_elb.ec2-elb.id})"
+	name		= "vpc-sg-ec2-elb-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
+	description	= "VPC Security Group controlling access to Project (${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}) Elastic LoadBalancer (${aws_elb.ec2-elb.id})"
 	tags {
-		Name = "VPC:SG-EC2:ELB-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
-		Resource = "ELB"
-		ResourceGroup = "EC2"
-		Ecosystem = "${var.project_ecosystem}"
-		Application = "${var.project_webapplication}"
-		Environment = "${var.project_environment}"
+		Name			= "VPC:SG-EC2:ELB-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
+		Resource		= "ELB"
+		ResourceGroup	= "EC2"
+		Ecosystem		= "${var.project_ecosystem}"
+		Application		= "${var.project_webapplication}"
+		Environment		= "${var.project_environment}"
 	}
 }
 
 resource "aws_security_group" "vpc-sg-ec2-inst" {
-	name = "vpc-sg-ec2-inst-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
-	description = "VPC Security Group controlling access to Project (${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}) EC2 Instances"
+	name		= "vpc-sg-ec2-inst-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
+	description	= "VPC Security Group controlling access to Project (${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}) EC2 Instances"
 	tags {
-		Name = "VPC:SG-EC2:INST-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
-		Resource = "Instance"
-		ResourceGroup = "EC2"
-		Ecosystem = "${var.project_ecosystem}"
-		Application = "${var.project_webapplication}"
-		Environment = "${var.project_environment}"
+		Name			= "VPC:SG-EC2:INST-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
+		Resource		= "Instance"
+		ResourceGroup	= "EC2"
+		Ecosystem		= "${var.project_ecosystem}"
+		Application		= "${var.project_webapplication}"
+		Environment		= "${var.project_environment}"
 	}
 }
