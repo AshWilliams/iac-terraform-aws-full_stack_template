6b45c0342ba6f0284ed9cf26d2cc794de1032795
Georgios Fisaris <gfisaris@gmail.com>
2016-09-28 11:14:04 +0100

    Add VPC Security Group for EC2 Instances

--- aws-vpc-security_groups.tf
+++ aws-vpc-security_groups.tf
@@ -1,12 +1,13 @@
-resource "aws_security_group" "vpc-sg-elb" {
+/*
+resource "aws_security_group" "demo-sg" {
 
-	name = "vpc-sg-ec2-elb-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
+	#name = ""
 	#(Optional, Forces new resource) The name of the security group. If omitted, Terraform will assign a random, unique name
 	
 	#name_prefix = ""
 	#(Optional, Forces new resource) Creates a unique name beginning with the specified prefix. Conflicts with name.
 	
-	description = "VPC Security Group controlling access to Project (${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}) Elastic LoadBalancer (${aws_elb.ec2-elb.id})"
+	#description = ""
 	#(Optional, Forces new resource) The security group description. Defaults to "Managed by Terraform". Cannot be "". NOTE: This field maps to the AWS GroupDescription attribute, for which there is no Update API. If you'd like to classify your security groups in a way that can be updated, use tags.
 
 	#vpc_id = ""
@@ -15,6 +16,12 @@ resource "aws_security_group" "vpc-sg-elb" {
 	#tags {}
 	#(Optional) A mapping of tags to assign to the resource.
 	
+}
+*/
+
+resource "aws_security_group" "vpc-sg-ec2-elb" {
+	name = "vpc-sg-ec2-elb-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
+	description = "VPC Security Group controlling access to Project (${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}) Elastic LoadBalancer (${aws_elb.ec2-elb.id})"
 	tags {
 		Name = "VPC:SG-EC2:ELB-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
 		Resource = "ELB"
@@ -23,5 +30,17 @@ resource "aws_security_group" "vpc-sg-elb" {
 		Application = "${var.project_webapplication}"
 		Environment = "${var.project_environment}"
 	}
-	
+}
+
+resource "aws_security_group" "vpc-sg-ec2-inst" {
+	name = "vpc-sg-ec2-inst-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
+	description = "VPC Security Group controlling access to Project (${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}) EC2 Instances"
+	tags {
+		Name = "VPC:SG-EC2:INST-${var.project_environment}-${var.project_ecosystem}-${var.project_webapplication}"
+		Resource = "Instance"
+		ResourceGroup = "EC2"
+		Ecosystem = "${var.project_ecosystem}"
+		Application = "${var.project_webapplication}"
+		Environment = "${var.project_environment}"
+	}
 }
