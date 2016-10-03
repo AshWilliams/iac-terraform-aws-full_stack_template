69987100d61f1710e139a773a9d235f8cc39bd50
Georgios Fisaris <gfisaris@gmail.com>
2016-10-03 11:52:48 +0100

    Update Documentation for Datasource AWS Services IP Ranges

--- aws-data_sources-ip_ranges.tf
+++ aws-data_sources-ip_ranges.tf
@@ -1,24 +1,23 @@
 /*
-data "aws_ip_ranges" "european_ec2" {
-  regions = [ "eu-west-1", "eu-central-1" ]
-  services = [ "ec2" ]
-}
+AWS_IP_RANGES
+Use this data source to get the IP ranges of various AWS products and services.
 
-resource "aws_security_group" "from_europe" {
+## Arguments Reference ##
+The following arguments are supported:
 
-  name = "from_europe"
+	data "aws_ip_ranges" "european_ec2" {
+		regions = []
+		#(Optional) Filter IP ranges by regions (or include all regions, if omitted). Valid items are global (for cloudfront) as well as all AWS regions (e.g. eu-central-1)
+		
+		services = []
+		#(Required) Filter IP ranges by services. Valid items are amazon (for amazon.com), cloudfront, ec2, route53 and route53_healthchecks.
+	}
 
-  ingress {
-    from_port = "443"
-    to_port = "443"
-    protocol = "tcp"
-    cidr_blocks = [ "${data.aws_ip_ranges.european_ec2.cidr_blocks}" ]
-  }
+## Attributes Reference ##
+The following attributes are exported:
 
-  tags {
-    CreateDate = "${data.aws_ip_ranges.european_ec2.create_date}"
-    SyncToken = "${data.aws_ip_ranges.european_ec2.sync_token}"
-  }
-
-}
+	cidr_blocks	- The lexically ordered list of CIDR blocks.
+	create_date	- The publication time of the IP ranges (e.g. 2016-08-03-23-46-05).
+	sync_token	- The publication time of the IP ranges, in Unix epoch time format (e.g. 1470267965).
+	
 */
