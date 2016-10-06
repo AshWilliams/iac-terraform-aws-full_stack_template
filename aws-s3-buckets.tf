7cfbf4f0d9528af184eb70b90e33ea703814d0f7
Georgios Fisaris <gfisaris@gmail.com>
2016-10-06 12:18:37 +0100

    * resource 'aws_s3_bucket.prj-s3_bucket-elb_logs' config: unknown variable referenced

--- aws-s3-buckets.tf
+++ aws-s3-buckets.tf
@@ -1,12 +1,5 @@
-resource "aws_s3_bucket" "prj-s3_bucket-elb_logs" {
-
-	bucket = "logs-ec2-elb-${var.project_ecosystem}-${var.project_webapplication}"
-	#(Required) The name of the bucket.
-	#A bucket name cannot contain the character '_'.
-	
-	acl = "private"
-	#(Optional) The canned ACL to apply. Defaults to "private".
-	
-	force_destroy = "true"
-	#(Optional, Default:false ) A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable.
+resource "aws_s3_bucket" "elb_logs" {
+  bucket        = "logs-ec2-elb-${var.prj_ecosystem}-${var.prj_application}"
+  acl           = "private"
+  force_destroy = "true"
 }
