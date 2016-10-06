46d8a7317feb80b9f1e55967d6cab5f4ad763cff
Georgios Fisaris <gfisaris@gmail.com>
2016-10-06 12:37:47 +0100

    * resource 'aws_iam_access_key.prj-iam_accesskey' config: unknown resource 'aws_iam_user.prj-iam_user' referenced in variable aws_iam_user.p

--- aws-iam-access_keys.tf
+++ aws-iam-access_keys.tf
@@ -1,6 +1,3 @@
-resource "aws_iam_access_key" "prj-iam_accesskey" {
-
-    user = "${aws_iam_user.prj-iam_user.name}"
-	#(Required) The IAM user to associate with this access key.
-
+resource "aws_iam_access_key" "master" {
+  user = "${aws_iam_user.prj_user.name}"
 }
