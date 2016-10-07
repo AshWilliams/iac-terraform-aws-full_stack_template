394423a893acaa3b9f326d5256f229f50eb91266
Georgios Fisaris <gfisaris@gmail.com>
2016-10-07 14:49:19 +0100

    * template_file.node_userdata: failed to render : 17:22: unknown variable accessed: var.aws_access_key

--- terraform-template_files.tf
+++ terraform-template_files.tf
@@ -1,3 +1,8 @@
 resource "template_file" "node_userdata" {
   template  = "${file("terraform-template_file-ec2-instance-userdata.txt")}"
+  vars {
+    iamAccessKey  = "${var.aws_access_key}"
+    iamSecretKey  = "${var.aws_secret_key}"
+    activeRegion  = "${var.aws_region}"
+  }
 }
