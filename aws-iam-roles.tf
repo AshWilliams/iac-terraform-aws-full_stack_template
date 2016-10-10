222d99b256c23d3889ebb4361097ec0317a09885
Georgios Fisaris <gfisaris@gmail.com>
2016-10-10 11:21:08 +0100

    * aws_iam_role.codedeploy: "assume_role_policy": required field is not set

--- aws-iam-roles.tf
+++ aws-iam-roles.tf
@@ -1,3 +1,28 @@
 resource "aws_iam_role" "codedeploy" {
   name = "iam-role-dtcd-${var.prj_ecosystem}-${var.prj_application}-${var.prj_environment}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:CompleteLifecycleAction",
+        "autoscaling:DeleteLifecycleHook",
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeLifecycleHooks",
+        "autoscaling:PutLifecycleHook",
+        "autoscaling:RecordLifecycleActionHeartbeat",
+        "ec2:DescribeInstances",
+        "ec2:DescribeInstanceStatus",
+        "tag:GetTags",
+        "tag:GetResources",
+        "sns:Publish",
+        "cloudwatch:DescribeAlarms"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
