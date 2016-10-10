d61ee3b205a4e4b9e0927199036c9dc04b48e5b1
Georgios Fisaris <gfisaris@gmail.com>
2016-10-10 10:48:02 +0100

    Add Resource: AWS DevTools CodeDeploy Groups

--- aws-devtools-codedeploy_apps.tf
+++ aws-devtools-codedeploy_apps.tf
@@ -1,3 +1,3 @@
 resource "aws_codedeploy_app" "webapp" {
-  name = "devtools-cd-${var.prj_ecosystem}-${var.prj_application}-${var.prj_environment}""
+  name = "devtools-cdapp-${var.prj_ecosystem}-${var.prj_application}-${var.prj_environment}"
 }
