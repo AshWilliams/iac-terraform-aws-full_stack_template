e7937f0804454c2b562dde1691c199fadf0edde5
Georgios Fisaris <gfisaris@gmail.com>
2016-10-12 16:39:22 +0100

    Update aws-vpc-subnets.tf

--- aws-vpc-subnets.tf
+++ aws-vpc-subnets.tf
@@ -1,5 +1,9 @@
+###########################
+##  Public VPC Subnets  ##
+###########################
+
 resource "aws_subnet" "pub01" {
-  count             = "${var.aws_vpc_sub_pub01_enabled}"
+  count             = "${var.aws_vpc_pub_enabled}"
   vpc_id            = "${aws_vpc.public.id}"
   cidr_block        = "${var.aws_vpc_subnet_pub01_cidr_block}"
   availability_zone = "${data.aws_availability_zones.available.names[0]}"
@@ -19,7 +23,7 @@ resource "aws_subnet" "pub01" {
 }
 
 resource "aws_subnet" "pub02" {
-  count             = "${var.aws_vpc_sub_pub02_enabled}"
+  count             = "${var.aws_vpc_pub_enabled}"
   vpc_id            = "${aws_vpc.public.id}"
   cidr_block        = "${var.aws_vpc_subnet_pub02_cidr_block}"
   availability_zone = "${data.aws_availability_zones.available.names[1]}"
@@ -39,7 +43,7 @@ resource "aws_subnet" "pub02" {
 }
 
 resource "aws_subnet" "pub03" {
-  count             = "${var.aws_vpc_sub_pub03_enabled}"
+  count             = "${var.aws_vpc_pub_enabled}"
   vpc_id            = "${aws_vpc.public.id}"
   cidr_block        = "${var.aws_vpc_subnet_pub03_cidr_block}"
   availability_zone = "${data.aws_availability_zones.available.names[1]}"
@@ -58,18 +62,56 @@ resource "aws_subnet" "pub03" {
   }
 }
 
-resource "aws_subnet" "pub04" {
-  count             = "${var.aws_vpc_sub_pub04_enabled}"
-  vpc_id            = "${aws_vpc.public.id}"
-  cidr_block        = "${var.aws_vpc_subnet_pub04_cidr_block}"
-  availability_zone = "${data.aws_availability_zones.available.names[2]}"
-  
-  map_public_ip_on_launch = "true"
+###########################
+##  Private VPC Subnets  ##
+###########################
+
+resource "aws_subnet" "prv01" {
+  count             = "${var.aws_vpc_prv_enabled}"
+  vpc_id            = "${aws_vpc.private.id}"
+  cidr_block        = "${var.aws_vpc_subnet_prv01_cidr_block}"
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
   
   tags {
-    Name          = "VPC-SUB-Pub04-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
-    AZ            = "${data.aws_availability_zones.available.names[2]}"
-    Type          = "Public"
+    Name          = "VPC-SUB-Prv01-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
+    AZ            = "${data.aws_availability_zones.available.names[0]}"
+    Type          = "Private"
+    Resource      = "Subnets"
+    ResourceGroup = "VPC"
+    Ecosystem     = "${var.prj_ecosystem}"
+    Application   = "${var.prj_application}"
+    Environment   = "${var.prj_environment}"
+  }
+}
+
+resource "aws_subnet" "prv02" {
+  count             = "${var.aws_vpc_prv_enabled}"
+  vpc_id            = "${aws_vpc.private.id}"
+  cidr_block        = "${var.aws_vpc_subnet_prv02_cidr_block}"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  
+  tags {
+    Name          = "VPC-SUB-Prv02-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
+    AZ            = "${data.aws_availability_zones.available.names[1]}"
+    Type          = "Private"
+    Resource      = "Subnets"
+    ResourceGroup = "VPC"
+    Ecosystem     = "${var.prj_ecosystem}"
+    Application   = "${var.prj_application}"
+    Environment   = "${var.prj_environment}"
+  }
+}
+
+resource "aws_subnet" "prv03" {
+  count             = "${var.aws_vpc_prv_enabled}"
+  vpc_id            = "${aws_vpc.private.id}"
+  cidr_block        = "${var.aws_vpc_subnet_prv03_cidr_block}"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  
+  tags {
+    Name          = "VPC-SUB-Prv03-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
+    AZ            = "${data.aws_availability_zones.available.names[1]}"
+    Type          = "Private"
     Resource      = "Subnets"
     ResourceGroup = "VPC"
     Ecosystem     = "${var.prj_ecosystem}"
