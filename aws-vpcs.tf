16e6ca9d5aed69f4703077bfa8c6c8ec520b0129
Georgios Fisaris <gfisaris@gmail.com>
2016-10-12 14:25:39 +0100

    Replace hardcoded values with variables

--- aws-vpcs.tf
+++ aws-vpcs.tf
@@ -1,13 +1,15 @@
-resource "aws_vpc" "master" {
-  cidr_block = "192.168.10.0/24"
-  enable_dns_support    = "true"
-  enable_dns_hostnames  = "true"
-  enable_classiclink    = "false"
-  
+resource "aws_vpc" "public" {
+  count                 = "${var.aws_vpc_pub_enabled}"
+  cidr_block            = "${var.aws_vpc_pub_cidr_block}"
+  enable_dns_support    = "${var.aws_vpc_pub_enable_dns_support}"
+  enable_dns_hostnames  = "${var.aws_vpc_pub_enable_dns_hostnames}"
+  enable_classiclink    = "${var.aws_vpc_pub_enable_classiclink}"
+
 	tags {
-		Name          = "EFS-Volume-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
-		Resource      = "Volume"
-		ResourceGroup = "EFS"
+		Name          = "VPC-pub-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
+    Type          = "Public"
+		Resource      = "VPC"
+		ResourceGroup = "VPC"
 		Ecosystem     = "${var.prj_ecosystem}"
 		Application   = "${var.prj_application}"
 		Environment   = "${var.prj_environment}"
