47f29cfa1b547e11333a59a61267b29d9befc60d
Georgios Fisaris <gfisaris@gmail.com>
2016-10-12 16:41:30 +0100

    Update aws-vpcs.tf

--- aws-vpcs.tf
+++ aws-vpcs.tf
@@ -5,13 +5,31 @@ resource "aws_vpc" "public" {
   enable_dns_hostnames  = "${var.aws_vpc_pub_enable_dns_hostnames}"
   enable_classiclink    = "${var.aws_vpc_pub_enable_classiclink}"
 
-	tags {
-		Name          = "VPC-pub-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
+  tags {
+    Name          = "VPC-Pub-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
     Type          = "Public"
-		Resource      = "VPC"
-		ResourceGroup = "VPC"
-		Ecosystem     = "${var.prj_ecosystem}"
-		Application   = "${var.prj_application}"
-		Environment   = "${var.prj_environment}"
+    Resource      = "VPC"
+    ResourceGroup = "VPC"
+    Ecosystem     = "${var.prj_ecosystem}"
+    Application   = "${var.prj_application}"
+    Environment   = "${var.prj_environment}"
+	}
+}
+
+resource "aws_vpc" "private" {
+  count                 = "${var.aws_vpc_prv_enabled}"
+  cidr_block            = "${var.aws_vpc_prv_cidr_block}"
+  enable_dns_support    = "${var.aws_vpc_prv_enable_dns_support}"
+  enable_dns_hostnames  = "${var.aws_vpc_prv_enable_dns_hostnames}"
+  enable_classiclink    = "${var.aws_vpc_prv_enable_classiclink}"
+
+  tags {
+    Name          = "VPC-Prv-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
+    Type          = "Private"
+    Resource      = "VPC"
+    ResourceGroup = "VPC"
+    Ecosystem     = "${var.prj_ecosystem}"
+    Application   = "${var.prj_application}"
+    Environment   = "${var.prj_environment}"
 	}
 }
