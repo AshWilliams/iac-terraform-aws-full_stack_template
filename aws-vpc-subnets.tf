8b2f2373fe99a0a4698e375501ab165824449500
Georgios Fisaris <gfisaris@gmail.com>
2016-10-12 14:58:27 +0100

    Update aws-vpc-subnets.tf

--- aws-vpc-subnets.tf
+++ aws-vpc-subnets.tf
@@ -42,12 +42,32 @@ resource "aws_subnet" "pub03" {
   count             = "${var.aws_vpc_sub_pub03_enabled}"
   vpc_id            = "${aws_vpc.public.id}"
   cidr_block        = "${var.aws_vpc_subnet_pub03_cidr_block}"
-  availability_zone = "${data.aws_availability_zones.available.names[2]}"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
   
   map_public_ip_on_launch = "true"
   
   tags {
     Name          = "VPC-SUB-Pub03-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
+    AZ            = "${data.aws_availability_zones.available.names[1]}"
+    Type          = "Public"
+    Resource      = "Subnets"
+    ResourceGroup = "VPC"
+    Ecosystem     = "${var.prj_ecosystem}"
+    Application   = "${var.prj_application}"
+    Environment   = "${var.prj_environment}"
+  }
+}
+
+resource "aws_subnet" "pub04" {
+  count             = "${var.aws_vpc_sub_pub04_enabled}"
+  vpc_id            = "${aws_vpc.public.id}"
+  cidr_block        = "${var.aws_vpc_subnet_pub04_cidr_block}"
+  availability_zone = "${data.aws_availability_zones.available.names[2]}"
+  
+  map_public_ip_on_launch = "true"
+  
+  tags {
+    Name          = "VPC-SUB-Pub04-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
     AZ            = "${data.aws_availability_zones.available.names[2]}"
     Type          = "Public"
     Resource      = "Subnets"
