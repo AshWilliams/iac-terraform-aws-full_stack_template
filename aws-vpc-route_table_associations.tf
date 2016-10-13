cb8b5738c32ef06c2a22303d07f4b19ead4f3392
Georgios Fisaris <gfisaris@gmail.com>
2016-10-13 13:16:42 +0100

    Update aws-vpc-route_table_associations.tf

--- aws-vpc-route_table_associations.tf
+++ aws-vpc-route_table_associations.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table_association" "rta_public" {
   count           = "${var.aws_vpc_pub_enabled * var.aws_vpc_pub_subnets_count}"
-  subnet_id       = ["${aws_subnet.pub_vpc_subnets[count.index]}"]
+  subnet_id       = "${aws_subnet.pub_vpc_subnets[count.index]}"
   route_table_id  = "${aws_route_table.public.id}"
 }
