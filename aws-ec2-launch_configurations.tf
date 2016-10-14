e10734cda7417584119768cc140c07c89e699a72
Georgios Fisaris <gfisaris@gmail.com>
2016-10-14 14:44:22 +0100

    Update aws-ec2-launch_configurations.tf

--- aws-ec2-launch_configurations.tf
+++ aws-ec2-launch_configurations.tf
@@ -1,4 +1,6 @@
 resource "aws_launch_configuration" "node" {
+  depends_on = ["aws_key_pair.master", "template_file.node_userdata", "aws_iam_role.ec2-instance-profile"]
+  
   name = "ec2-lc-${var.prj_ecosystem}-${var.prj_application}"
   
   image_id      = "${data.aws_ami.centos7.id}"
