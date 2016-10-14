de41e464a7c950ab947592587c5553d298ce7824
Georgios Fisaris <gfisaris@gmail.com>
2016-10-14 16:35:54 +0100

    Update terraform-template_files.tf

--- terraform-template_files.tf
+++ terraform-template_files.tf
@@ -6,3 +6,11 @@ resource "template_file" "node_userdata" {
     activeRegion  = "${var.aws_region}"
   }
 }
+
+resource "template_file" "appuser_iam_crd" {
+  template = "${file("files-appuser_iam_crd")}"
+  vars {
+    iamAccKey = "${aws_iam_access_key.master.id}"
+    iamSecKey = "${aws_iam_access_key.master.secret}"
+  }
+}
