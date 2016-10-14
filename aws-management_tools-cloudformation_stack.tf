f544efc656c6e3fa483684d237a96760f33f67a1
Georgios Fisaris <gfisaris@gmail.com>
2016-10-14 11:29:00 +0100

    "template_body" contains an invalid JSON: invalid character ':' after top-level value

--- aws-management_tools-cloudformation_stack.tf
+++ aws-management_tools-cloudformation_stack.tf
@@ -1,26 +1,30 @@
 resource "aws_cloudformation_stack" "codedeploy-app-revision" {
-
   name = "codedeploy-deployment-groups-demo"
+  
   template_body = <<STACK
-"DeploymentGroup" : {
-  "Type" : "AWS::CodeDeploy::DeploymentGroup",
-  "Properties" : {
-    "ApplicationName" : "devtools-cdapp-demoeco-demoapp-stg",
-    "AutoScalingGroups" : [ "EC2.ASG | demoeco | demoapp | stg | NO Advance Metrics" ],
-    "Deployment" : {
-      "Description" : "First time",
-      "IgnoreApplicationStopFailures" : "true",
-      "Revision" : {
-        "RevisionType" : "S3",
-        "S3Location" : {
-          "Bucket" : "aws.codedeploy",
-          "Key" : "20160610REV-asgGEMApps.zip",
-          "BundleType" : "Zip",
-          "ETag" : "927251a220966bcf3fe9b5a389f9c73f"
+{
+  "DeploymentGroup":{
+    "Type":"AWS::CodeDeploy::DeploymentGroup",
+    "Properties":{
+      "ApplicationName":"devtools-cdapp-demoeco-demoapp-stg",
+      "AutoScalingGroups":[
+        "EC2.ASG | demoeco | demoapp | stg | NO Advance Metrics"
+      ],
+      "Deployment":{
+        "Description":"First time",
+        "IgnoreApplicationStopFailures":"true",
+        "Revision":{
+          "RevisionType":"S3",
+          "S3Location":{
+            "Bucket":"aws.codedeploy",
+            "Key":"20160610REV-asgGEMApps.zip",
+            "BundleType":"Zip",
+            "ETag":"927251a220966bcf3fe9b5a389f9c73f"
+          }
         }
-      }
-    },
-    "ServiceRoleArn" : "arn:aws:iam::547013329349:role/iam-role-dtcd-demoeco-demoapp-stg"
+      },
+      "ServiceRoleArn":"arn:aws:iam::547013329349:role/iam-role-dtcd-demoeco-demoapp-stg"
+    }
   }
 }
 STACK
