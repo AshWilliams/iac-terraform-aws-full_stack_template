ca8cd674e8ecd10223a0c5736760962ad67dd6ca
Georgios Fisaris <gfisaris@gmail.com>
2016-10-14 12:26:00 +0100

    Creating CloudFormation stack failed: ValidationError: Invalid template property or properties [Type, Properties]

--- aws-management_tools-cloudformation_stack.tf
+++ aws-management_tools-cloudformation_stack.tf
@@ -1,30 +1,36 @@
 resource "aws_cloudformation_stack" "codedeploy-app-revision" {
   name = "codedeploy-deployment-groups-demo"
-  
+  on_failure = "DO_NOTHING"
   template_body = <<STACK
 {
-  "Type":"AWS::CodeDeploy::DeploymentGroup",
-  "Properties":{
-    "ApplicationName":"devtools-cdapp-demoeco-demoapp-stg",
-    "DeploymentGroupName":"devtools-cdgrp-demoeco-demoapp-cloudf",
-    "DeploymentConfigName":"CodeDeployDefault.OneAtATime",
-    "AutoScalingGroups":[
-      "EC2.ASG | demoeco | demoapp | stg | NO Advance Metrics"
-    ],
-    "Deployment":{
-      "Description":"First time",
-      "IgnoreApplicationStopFailures":"true",
-      "Revision":{
-        "RevisionType":"S3",
-        "S3Location":{
-          "Bucket":"aws.codedeploy",
-          "Key":"20160610REV-asgGEMApps.zip",
-          "BundleType":"Zip",
-          "ETag":"927251a220966bcf3fe9b5a389f9c73f"
-        }
+  "AWSTemplateFormatVersion":"2010-09-09",
+  "Description":"CloudFormation JSON Script that will launch an EC2 Instance behind an Elastic LoadBalancer ",
+  "Resources":{
+    "cdgrpDemoApp":{
+      "Type":"AWS::CodeDeploy::DeploymentGroup",
+      "Properties":{
+        "ApplicationName":"devtools-cdapp-demoeco-demoapp-stg",
+        "DeploymentGroupName":"devtools-cdgrp-demoeco-demoapp-cloudf",
+        "DeploymentConfigName":"CodeDeployDefault.OneAtATime",
+        "AutoScalingGroups":[
+          "EC2.ASG | demoeco | demoapp | stg | NO Advance Metrics"
+        ],
+        "Deployment":{
+          "Description":"First time",
+          "IgnoreApplicationStopFailures":"true",
+          "Revision":{
+            "RevisionType":"S3",
+            "S3Location":{
+              "Bucket":"aws.codedeploy",
+              "Key":"20160610REV-asgGEMApps.zip",
+              "BundleType":"Zip",
+              "ETag":"927251a220966bcf3fe9b5a389f9c73f"
+            }
+          }
+        },
+        "ServiceRoleArn":"arn:aws:iam::547013329349:role/iam-role-dtcd-demoeco-demoapp-stg"
       }
-    },
-    "ServiceRoleArn":"arn:aws:iam::547013329349:role/iam-role-dtcd-demoeco-demoapp-stg"
+    }
   }
 }
 STACK
