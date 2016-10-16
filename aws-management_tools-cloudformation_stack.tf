17b7c81e340a8f3752d54a61349ec6c32cc41404
Georgios Fisaris <gfisaris@gmail.com>
2016-10-16 04:37:26 +0100

    Add Resource AWS CloudFormation Stack: CodeDeploy App Rev FROM GitHub

--- aws-management_tools-cloudformation_stack.tf
+++ aws-management_tools-cloudformation_stack.tf
@@ -1,3 +1,42 @@
+resource "aws_cloudformation_stack" "codedeploy-app-revision-github" {
+  depends_on = ["aws_codedeploy_app.webapp", "aws_autoscaling_group.asg", "aws_autoscaling_notification.asg", "aws_iam_role.ec2-instance-profile", "aws_iam_role.codedeploy", "aws_efs_file_system.volume", "aws_efs_mount_target.az-mnt"]
+  name = "codedeploy-deployment-groups-demo"
+  on_failure = "DO_NOTHING"
+  template_body = <<STACK
+{
+  "AWSTemplateFormatVersion":"2010-09-09",
+  "Description":"CloudFormation JSON Script that will launch an EC2 Instance behind an Elastic LoadBalancer ",
+  "Resources":{
+    "cdgrpDemoApp":{
+      "Type":"AWS::CodeDeploy::DeploymentGroup",
+      "Properties":{
+        "ApplicationName":"devtools-cdapp-demoeco-demoapp-stg",
+        "DeploymentGroupName":"devtools-cdgrp-demoeco-demoapp-cloudf",
+        "DeploymentConfigName":"CodeDeployDefault.OneAtATime",
+        "AutoScalingGroups":[
+          "EC2.ASG | demoeco | demoapp | stg | NO Advance Metrics"
+        ],
+        "Deployment":{
+          "Description":"First time",
+          "IgnoreApplicationStopFailures":"true",
+          "Revision":{
+            "RevisionType":"GitHub",
+            "GitHubLocation":{
+              "CommitId":"c15ba5caa6a079d73b83d9ab3f2a2872ffe16b1c",
+              "Repository":"gfisaris/aws-codedeploy-app-revision"
+            }
+          }
+        },
+        "ServiceRoleArn":"${aws_iam_role.codedeploy.arn}"
+      }
+    }
+  }
+}
+STACK
+}
+
+
+/*
 resource "aws_cloudformation_stack" "codedeploy-app-revision" {
   depends_on = ["aws_codedeploy_app.webapp", "aws_autoscaling_group.asg", "aws_autoscaling_notification.asg", "aws_iam_role.ec2-instance-profile", "aws_iam_role.codedeploy", "aws_efs_file_system.volume", "aws_efs_mount_target.az-mnt"]
   name = "codedeploy-deployment-groups-demo"
@@ -36,3 +75,4 @@ resource "aws_cloudformation_stack" "codedeploy-app-revision" {
 }
 STACK
 }
+*/
