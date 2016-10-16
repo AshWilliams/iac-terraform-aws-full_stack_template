fdbfe7f01ef3397ad652f4c999a25f29eeffe341
Georgios Fisaris <gfisaris@gmail.com>
2016-10-16 23:17:01 +0100

    Update aws-s3-bucket_object.tf

--- aws-s3-bucket_object.tf
+++ aws-s3-bucket_object.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_object" "appuser_iam_crd" {
   bucket = "${aws_s3_bucket.app.id}"
-  key = "helloworld"
+  key = ".app-iam_user.credentials"
   content = "${template_file.appuser_iam_crd.rendered}"
 }
