1a45deb0467a8b3a0eec783978709caa3f4a4897
Georgios Fisaris <gfisaris@gmail.com>
2016-10-17 16:00:58 +0100

    Update aws-s3-bucket_objects.tf

--- aws-s3-bucket_objects.tf
+++ aws-s3-bucket_objects.tf
@@ -10,25 +10,27 @@ resource "aws_s3_bucket_object" "app-github_repo" {
   content = "${template_file.app-github_repo.rendered}"
 }
 
-resource "aws_s3_bucket_object" "app-nginx_conf" {
-  bucket = "${aws_s3_bucket.app.id}"
-  key = "app-nginx.conf"
-  content = "${template_file.app-nginx_conf.rendered}"
-}
+## Configuration Files for Application vHost
 
-resource "aws_s3_bucket_object" "app-phpfpm_conf" {
-  bucket = "${aws_s3_bucket.app.id}"
-  key = "app-phpfpm.conf"
-  content = "${template_file.app-phpfpm_conf.rendered}"
-}
+  resource "aws_s3_bucket_object" "vhost-nginx_conf" {
+    bucket = "${aws_s3_bucket.app.id}"
+    key = "vhost-nginx_conf"
+    content = "${template_file.vhost-nginx_conf.rendered}"
+  }
 
-resource "aws_s3_bucket_object" "app-newrelic_ini" {
-  bucket = "${aws_s3_bucket.app.id}"
-  key = "app-newrelic.ini"
-  content = "${template_file.app-newrelic_ini.rendered}"
-}
+  resource "aws_s3_bucket_object" "vhost-phpfpm_conf" {
+    bucket = "${aws_s3_bucket.app.id}"
+    key = "vhost-phpfpm_conf"
+    content = "${template_file.vhost-phpfpm_conf.rendered}"
+  }
 
-## Configuration Files for Cloudwatch Logs Monitoring
+  resource "aws_s3_bucket_object" "vhost-newrelic_conf" {
+    bucket = "${aws_s3_bucket.app.id}"
+    key = "vhost-newrelic_conf"
+    content = "${template_file.vhost-newrelic_conf.rendered}"
+  }
+
+## Configuration Files for Cloudwatch Logs Monitoring Service
 
   resource "aws_s3_bucket_object" "cloudwatch-allInstances_app_logs_conf" {
     bucket  = "${aws_s3_bucket.app.id}"
