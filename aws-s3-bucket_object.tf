fda3a9299948ca6594c6bade047b6c2fdde7c844
Georgios Fisaris <gfisaris@gmail.com>
2016-10-17 12:18:56 +0100

    Update aws-s3-bucket_object.tf

--- aws-s3-bucket_object.tf
+++ aws-s3-bucket_object.tf
@@ -1,5 +1,11 @@
-resource "aws_s3_bucket_object" "appuser_iam_crd" {
+resource "aws_s3_bucket_object" "app-iam_user_crd" {
   bucket = "${aws_s3_bucket.app.id}"
   key = ".app-iam_user.credentials"
-  content = "${template_file.appuser_iam_crd.rendered}"
+  content = "${template_file.app-iam_user_crd.rendered}"
+}
+
+resource "aws_s3_bucket_object" "app_github_repo" {
+  bucket = "${aws_s3_bucket.app.id}"
+  key = ".app-github.repo"
+  content = "${template_file.app-github_repo.rendered}"
 }
