ea06cd318673809dbbde608cf96722b3d834c299
Georgios Fisaris <gfisaris@gmail.com>
2016-10-17 15:05:59 +0100

    Update and rename aws-s3-bucket_object.tf to aws-s3-bucket_objects.tf

---
+++ aws-s3-bucket_objects.tf
@@ -0,0 +1,29 @@
+resource "aws_s3_bucket_object" "app-iam_user_crd" {
+  bucket = "${aws_s3_bucket.app.id}"
+  key = ".app-iam_user.credentials"
+  content = "${template_file.app-iam_user_crd.rendered}"
+}
+
+resource "aws_s3_bucket_object" "app-github_repo" {
+  bucket = "${aws_s3_bucket.app.id}"
+  key = ".app-github.repo"
+  content = "${template_file.app-github_repo.rendered}"
+}
+
+resource "aws_s3_bucket_object" "app-nginx_conf" {
+  bucket = "${aws_s3_bucket.app.id}"
+  key = "app-nginx.conf"
+  content = "${template_file.app-nginx_conf.rendered}"
+}
+
+resource "aws_s3_bucket_object" "app-phpfpm_conf" {
+  bucket = "${aws_s3_bucket.app.id}"
+  key = "app-phpfpm.conf"
+  content = "${template_file.app-phpfpm_conf.rendered}"
+}
+
+resource "aws_s3_bucket_object" "app-newrelic_ini" {
+  bucket = "${aws_s3_bucket.app.id}"
+  key = "app-newrelic.ini"
+  content = "${template_file.app-newrelic_ini.rendered}"
+}
