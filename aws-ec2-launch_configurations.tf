13d8995a99b0eada07ac3b804982c9de0cf040d4
Georgios Fisaris <gfisaris@gmail.com>
2016-10-18 13:10:45 +0100

    Update aws-ec2-launch_configurations.tf

--- aws-ec2-launch_configurations.tf
+++ aws-ec2-launch_configurations.tf
@@ -9,7 +9,7 @@ resource "aws_launch_configuration" "node" {
   key_name              = "${aws_key_pair.master.key_name}"
   iam_instance_profile = "${aws_iam_instance_profile.node.name}"
     
-  user_data = "${template_file.node_userdata.rendered}"
+  user_data = "${template_file.ec2-instance_userdata.rendered}"
   
   enable_monitoring = false
   
