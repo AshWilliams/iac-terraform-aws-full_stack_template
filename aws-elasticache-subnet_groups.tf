2dd17b38fbe630e451475816480557b7db0189ea
Georgios Fisaris <gfisaris@gmail.com>
2016-10-18 10:38:51 +0100

    Update aws-elasticache-subnet_groups.tf

--- aws-elasticache-subnet_groups.tf
+++ aws-elasticache-subnet_groups.tf
@@ -1,4 +1,5 @@
 resource "aws_elasticache_subnet_group" "redis" {
-  name = "${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
-  subnet_ids = ["${aws_subnet.public.id}"]
+  count       = "${var.aws_elasticache_cluster_redis_enabled}"
+  name        = "${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
+  subnet_ids  = ["${aws_subnet.public.id}"]
 }
