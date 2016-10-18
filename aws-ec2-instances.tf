43a0d9cb5ccb3705764d1d60f017d871f0d2860e
Georgios Fisaris <gfisaris@gmail.com>
2016-10-18 13:10:57 +0100

    Update aws-ec2-instances.tf

--- aws-ec2-instances.tf
+++ aws-ec2-instances.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2_instance" {
 	
     instance_type = "t2.micro"
 	
-	user_data = "${template_file.node_userdata.rendered}"
+	user_data = "${template_file.ec2-instance_userdata.rendered}"
 
 	root_block_device {
 		volume_type		= "gp2"
