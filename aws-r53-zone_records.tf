deeceea5e1a5d1d034346ac4c75079cb9f70f612
Georgios Fisaris <gfisaris@gmail.com>
2016-10-19 14:53:56 +0100

    Update and rename tpl-aws-r53-zone_records.tf.tpl to aws-r53-zone_records.tf

--- aws-r53-zone_records.tf
+++ aws-r53-zone_records.tf
@@ -1,18 +1,18 @@
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "main" {
    
-  zone_id = "${aws_route53_zone.domain.zone_id}"
+  zone_id = "${aws_route53_zone.domain_prv.zone_id}"
   #(Required) The ID of the hosted zone to contain this record.
    
-  name = "www.domain.tld"
+  name = "dedup.app"
   #(Required) The name of the record.
    
   type = "A"
   #(Required) The record type.
    
-  ttl = "300"
+ # ttl = "300"
   #(Required for non-alias records) The TTL of the record.
    
-  records = ["${aws_eip.lb.public_ip}"]
+  #records = ["${aws_eip.external.public_ip}"]
   #(Required for non-alias records) A string list of records.
 
   #set_identifier
@@ -21,7 +21,11 @@ resource "aws_route53_record" "www" {
   #health_check_id
   #(Optional) The health check the record should be associated with.
   
-  #alias
+  alias {
+    name = "${aws_elb.external.dns_name}"
+    zone_id = "${aws_elb.external.zone_id}"
+    evaluate_target_health = true
+  }
   #(Optional) An alias block. Conflicts with ttl & records. Alias record documented below.
   
   #failover_routing_policy
