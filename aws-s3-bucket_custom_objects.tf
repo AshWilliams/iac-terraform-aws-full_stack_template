eea5f6ae7e0b2a781339ac5f727167ba4aa93f97
Georgios Fisaris <gfisaris@gmail.com>
2016-10-19 14:52:50 +0100

    Update and rename tpl-aws-s3-bucket_custom_objects.tf.tpl to aws-s3-bucket_custom_objects.tf

--- aws-s3-bucket_custom_objects.tf
+++ aws-s3-bucket_custom_objects.tf
@@ -1,7 +1,7 @@
 ## Aplication Symfony Parameters yml
 
   resource "aws_s3_bucket_object" "app-symfony_parameters_yml" {
-    bucket = "${aws_s3_bucket.app.id}"
+    bucket = "${aws_s3_bucket.app-config.id}"
     key = "app-symfony_parameters_yml"
     content = "${template_file.app-symfony_parameters_yml.rendered}"
   }
