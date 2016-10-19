3328352758329df90c682492054bfcb6fd4fd350
Georgios Fisaris <gfisaris@gmail.com>
2016-10-19 12:27:12 +0100

    Update aws-s3-buckets.tf

--- aws-s3-buckets.tf
+++ aws-s3-buckets.tf
@@ -1,5 +1,11 @@
 resource "aws_s3_bucket" "app" {
-  bucket        = "${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
+  bucket        = "${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}-app"
+  acl           = "private"
+  force_destroy = "true"
+}
+
+resource "aws_s3_bucket" "app-config" {
+  bucket        = "${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}-app-cnf"
   acl           = "private"
   force_destroy = "true"
 }
