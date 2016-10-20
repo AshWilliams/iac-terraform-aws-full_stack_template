999df3e68934a51697e9b5f81e00097140f23ecb
Georgios Fisaris <gfisaris@gmail.com>
2016-10-20 12:15:41 +0100

    Update aws-vpc-public-security_group_rules.tf

--- aws-vpc-public-security_group_rules.tf
+++ aws-vpc-public-security_group_rules.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group_rule" "ec2-elb-ingress-port-80" {
-  security_group_id = "${aws_security_group.ec2-elb.id}"
+resource "aws_security_group_rule" "vpc-pub-ec2-elb-ingress-port-80" {
+  security_group_id = "${aws_security_group.vpc-pub-ec2-elb.id}"
   type        = "ingress"
   protocol    = "tcp"
   from_port   = "80"
@@ -7,8 +7,8 @@ resource "aws_security_group_rule" "ec2-elb-ingress-port-80" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "ec2-elb-egress-port-80" {
-  security_group_id	= "${aws_security_group.ec2-elb.id}"
+resource "aws_security_group_rule" "vpc-pub-ec2-elb-egress-port-80" {
+  security_group_id	= "${aws_security_group.vpc-pub-ec2-elb.id}"
   type        = "egress"
   protocol    = "tcp"
   from_port   = "0"
@@ -16,17 +16,17 @@ resource "aws_security_group_rule" "ec2-elb-egress-port-80" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "ec2-inst-ingress-elb-port-80" {
-  security_group_id = "${aws_security_group.ec2-inst.id}"
+resource "aws_security_group_rule" "vpc-pub-ec2-inst-ingress-elb-port-80" {
+  security_group_id = "${aws_security_group.vpc-pub-ec2-inst.id}"
   type        = "ingress"
   protocol    = "tcp"
   from_port   = "80"
   to_port     = "80"
-  source_security_group_id = "${aws_security_group.ec2-elb.id}"
+  source_security_group_id = "${aws_security_group.vpc-pub-ec2-elb.id}"
 }
 
-resource "aws_security_group_rule" "ec2-inst-egress-port-80" {
-  security_group_id = "${aws_security_group.ec2-inst.id}"
+resource "aws_security_group_rule" "vpc-pub-ec2-inst-egress-port-80" {
+  security_group_id = "${aws_security_group.vpc-pub-ec2-inst.id}"
   type        = "egress"
   protocol    = "tcp"
   from_port   = "0"
@@ -34,74 +34,74 @@ resource "aws_security_group_rule" "ec2-inst-egress-port-80" {
   cidr_blocks = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "elcache-memc-ingress-port-11211" {
-  security_group_id = "${aws_security_group.elcache-memcached.id}"
+resource "aws_security_group_rule" "vpc-pub-elcache-memc-ingress-port-11211" {
+  security_group_id = "${aws_security_group.vpc-pub-elcache-memcached.id}"
   type        = "ingress"
   protocol    = "tcp"
   from_port   = "11211"
   to_port     = "11211"
-  source_security_group_id = "${aws_security_group.ec2-inst.id}"
+  source_security_group_id = "${aws_security_group.vpc-pub-ec2-inst.id}"
 }
 
-resource "aws_security_group_rule" "elcache-memc-egress-port-11211" {
-  security_group_id = "${aws_security_group.elcache-memcached.id}"
+resource "aws_security_group_rule" "vpc-pub-elcache-memc-egress-port-11211" {
+  security_group_id = "${aws_security_group.vpc-pub-elcache-memcached.id}"
   type        = "egress"
   protocol    = "tcp"
   from_port   = "11211"
   to_port     = "11211"
-  source_security_group_id = "${aws_security_group.ec2-inst.id}"
+  source_security_group_id = "${aws_security_group.vpc-pub-ec2-inst.id}"
 }
 
-resource "aws_security_group_rule" "elcache-redis-ingress-port-6379" {
-  security_group_id = "${aws_security_group.elcache-redis.id}"
+resource "aws_security_group_rule" "vpc-pub-elcache-redis-ingress-port-6379" {
+  security_group_id = "${aws_security_group.vpc-pub-elcache-redis.id}"
   type        = "ingress"
   protocol    = "tcp"
   from_port   = "6379"
   to_port     = "6379"
-  source_security_group_id = "${aws_security_group.ec2-inst.id}"
+  source_security_group_id = "${aws_security_group.vpc-pub-ec2-inst.id}"
 }
 
-resource "aws_security_group_rule" "elcache-redis-egress-port-6379" {
-  security_group_id = "${aws_security_group.elcache-redis.id}"
+resource "aws_security_group_rule" "vpc-pub-elcache-redis-egress-port-6379" {
+  security_group_id = "${aws_security_group.vpc-pub-elcache-redis.id}"
   type        = "egress"
   protocol    = "tcp"
   from_port   = "6379"
   to_port     = "6379"
-  source_security_group_id = "${aws_security_group.ec2-inst.id}"
+  source_security_group_id = "${aws_security_group.vpc-pub-ec2-inst.id}"
 }
 
-resource "aws_security_group_rule" "efs-mnt-ingress-port-2049" {
-  security_group_id = "${aws_security_group.efs-mounts.id}"
+resource "aws_security_group_rule" "vpc-pub-efs-mnt-ingress-port-2049" {
+  security_group_id = "${aws_security_group.vpc-pub-efs-mounts.id}"
   type        = "ingress"
   protocol    = "tcp"
   from_port   = "2049"
   to_port     = "2049"
-  source_security_group_id = "${aws_security_group.ec2-inst.id}"
+  source_security_group_id = "${aws_security_group.vpc-pub-ec2-inst.id}"
 }
 
-resource "aws_security_group_rule" "efs-mnt-egress-port-2049" {
-  security_group_id = "${aws_security_group.efs-mounts.id}"
+resource "aws_security_group_rule" "vpc-pub-efs-mnt-egress-port-2049" {
+  security_group_id = "${aws_security_group.vpc-pub-efs-mounts.id}"
   type        = "egress"
   protocol    = "tcp"
   from_port   = "2049"
   to_port     = "2049"
-  source_security_group_id = "${aws_security_group.ec2-inst.id}"
+  source_security_group_id = "${aws_security_group.vpc-pub-ec2-inst.id}"
 }
 
-resource "aws_security_group_rule" "rds-inst-ingress-port-3306" {
-  security_group_id = "${aws_security_group.rds-instances.id}"
+resource "aws_security_group_rule" "vpc-pub-rds-inst-ingress-port-3306" {
+  security_group_id = "${aws_security_group.vpc-pub-rds-instances.id}"
   type        = "ingress"
   protocol    = "tcp"
   from_port   = "3306"
   to_port     = "3306"
-  source_security_group_id = "${aws_security_group.ec2-inst.id}"
+  source_security_group_id = "${aws_security_group.vpc-pub-ec2-inst.id}"
 }
 
-resource "aws_security_group_rule" "rds-inst-egress-port-3306" {
-  security_group_id = "${aws_security_group.rds-instances.id}"
+resource "aws_security_group_rule" "vpc-pub-rds-inst-egress-port-3306" {
+  security_group_id = "${aws_security_group.vpc-pub-rds-instances.id}"
   type        = "egress"
   protocol    = "tcp"
   from_port   = "3306"
   to_port     = "3306"
-  source_security_group_id = "${aws_security_group.ec2-inst.id}"
+  source_security_group_id = "${aws_security_group.vpc-pub-ec2-inst.id}"
 }
