044feb928fe2af0a905ef3b45167e35519875dbc
Georgios Fisaris <gfisaris@gmail.com>
2016-10-20 13:23:44 +0100

    Update aws-rds-db_instances.tf

--- aws-rds-db_instances.tf
+++ aws-rds-db_instances.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "mariadb" {
   multi_az                = "false"
   availability_zone       = "eu-west-1a"
   publicly_accessible     = "true"
-  db_subnet_group_name    = "${aws_db_subnet_group.default.id}"
+  db_subnet_group_name    = "${aws_db_subnet_group.services.id}"
   vpc_security_group_ids  = ["${aws_security_group.vpc-prv-rds-instances.id}"]
   
   name      = "${var.project_db_name}"
