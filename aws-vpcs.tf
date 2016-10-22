f561d8dd6c38aca00239c57e3df3e847fb3c60f8
Georgios Fisaris <gfisaris@gmail.com>
2016-10-22 11:41:25 +0100

    One (1) VPC

--- aws-vpcs.tf
+++ aws-vpcs.tf
@@ -1,35 +1,16 @@
-resource "aws_vpc" "public" {
-  count                 = "${var.aws_vpc_pub_enabled}"
-  cidr_block            = "${var.aws_vpc_pub_cidr_block}"
-  enable_dns_support    = "${var.aws_vpc_pub_enable_dns_support}"
-  enable_dns_hostnames  = "${var.aws_vpc_pub_enable_dns_hostnames}"
-  enable_classiclink    = "${var.aws_vpc_pub_enable_classiclink}"
-
+resource "aws_vpc" "app" {
+  count                 = "${var.aws_vpc_app_enabled}"
+  cidr_block            = "${var.aws_vpc_app_cidr_block}"
+  enable_classiclink    = "${var.aws_vpc_app_enable_classiclink}"
+  enable_dns_support    = "${var.aws_vpc_app_enable_dns_support}"
+  enable_dns_hostnames  = "${var.aws_vpc_app_enable_dns_hostnames}"
+  
   tags {
-    Name          = "VPC-Pub-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
-    Type          = "Public"
+    Name          = "VPC-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
     Resource      = "VPC"
     ResourceGroup = "VPC"
     Ecosystem     = "${var.prj_ecosystem}"
     Application   = "${var.prj_application}"
     Environment   = "${var.prj_environment}"
-	}
-}
-
-resource "aws_vpc" "private" {
-  count                 = "${var.aws_vpc_prv_enabled}"
-  cidr_block            = "${var.aws_vpc_prv_cidr_block}"
-  enable_dns_support    = "${var.aws_vpc_prv_enable_dns_support}"
-  enable_dns_hostnames  = "${var.aws_vpc_prv_enable_dns_hostnames}"
-  enable_classiclink    = "${var.aws_vpc_prv_enable_classiclink}"
-
-  tags {
-    Name          = "VPC-Prv-${var.prj_environment}-${var.prj_ecosystem}-${var.prj_application}"
-    Type          = "Private"
-    Resource      = "VPC"
-    ResourceGroup = "VPC"
-    Ecosystem     = "${var.prj_ecosystem}"
-    Application   = "${var.prj_application}"
-    Environment   = "${var.prj_environment}"
-	}
+  }
 }
